04ed47c2975f877e50400cc3e68de7f1c0044d94
Kyle H <kyle.henderson@bbc.co.uk>
2024-09-13 09:18:01 +0100

    Initial Commit

---
+++ FontComparison/FontComparisonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FontComparisonApp.swift
+//  FontComparison
+//
+//  Created by Kyle O Henderson on 13/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct FontComparisonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FontComparisonTests/FontComparisonTests.swift
@@ -0,0 +1,16 @@
+//
+//  FontComparisonTests.swift
+//  FontComparisonTests
+//
+//  Created by Kyle O Henderson on 13/09/2024.
+//
+
+import Testing
+
+struct FontComparisonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
